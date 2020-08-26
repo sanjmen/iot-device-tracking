2fb579aab3b4bc79042d269c54a0bfd1b8e6524b
santi <sanjmen@gmail.com>
2020-08-26 18:01:17 +0200

    emulate devices loading via REST api, closes #6

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <Map
-      v-if="!isLoading"
-      :center="{ lat, lng }"
-      :zoom="zoom"
-      :devices="devices"
-    ></Map>
+    <section v-if="errored">
+      <p>
+        We're sorry, we're not able to retrieve this information at the moment,
+        please try back later
+      </p>
+    </section>
+
+    <section v-else>
+      <div v-if="loading">Loading...</div>
+      <Map v-else :center="{ lat, lng }" :zoom="zoom" :devices="devices"></Map>
+    </section>
   </div>
 </template>
 
 <script>
-import devices from "./data/devices.json";
 import Map from "./components/Map.vue";
 
 export default {
@@ -23,18 +27,30 @@ export default {
       lat: 47.41322,
       lng: -1.219482,
       devices: [],
-      isLoading: true,
+      loading: true,
+      errored: false,
       zoom: 6
     };
   },
   mounted() {
-    this.fetchDevices();
-  },
-  methods: {
-    fetchDevices() {
-      this.devices = devices;
-      this.isLoading = false;
-    }
+    fetch(
+      "https://gist.githubusercontent.com/sanjmen/bd0ae432b98a746887dff5bc790818cd/raw/1d8363b1577e270af0765f8d99754a2b044874a3/devices.json"
+    )
+      .then(response => {
+        if (response.status === 200) {
+          response.json().then(data => {
+            this.devices = data;
+            this.isLoading = false;
+          });
+        } else {
+          this.errored = true;
+        }
+      })
+      // eslint-disable-next-line no-unused-vars
+      .catch(error => {
+        this.errored = true;
+      })
+      .finally(() => (this.loading = false));
   }
 };
 </script>
